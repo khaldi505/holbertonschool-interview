12eca3bb0710b634cade9e4b192b70aed07ee6b0
khaldi505 <habetlbrika69@gmail.com>
2021-01-10 09:53:54 +0100

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,19 +7,19 @@ this function will unlock boxes based on the keys inside of em
 
 
 def canUnlockAll(boxes):
-    unlocked = True
+
     length = len(boxes) - 1
     unlocked_boxes = []
     for f_keys in boxes[0]:
         if f_keys not in unlocked_boxes and f_keys <= length:
             unlocked_boxes.append(f_keys)
-    for x in range(length):
+    for x in range(length + 1):
         if x in unlocked_boxes:
             for y in boxes[x]:
                 if y not in unlocked_boxes and y <= length:
                     unlocked_boxes.append(y)
     if (len(unlocked_boxes) == length) or\
             (length == 0 and len(unlocked_boxes) == 1):
-        return unlocked
+        return True
     else:
         return False
