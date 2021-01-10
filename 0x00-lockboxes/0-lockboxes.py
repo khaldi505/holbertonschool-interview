3e23d0e095758a368cad9714facfab3d953ee51e
khaldi505 <habetlbrika69@gmail.com>
2021-01-10 09:25:15 +0100

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,14 +11,15 @@ def canUnlockAll(boxes):
     length = len(boxes) - 1
     unlocked_boxes = []
     for f_keys in boxes[0]:
-        if not f_keys in unlocked_boxes and not f_keys > length:
+        if f_keys not in unlocked_boxes and f_keys <= length:
             unlocked_boxes.append(f_keys)
     for x in range(length):
         if x in unlocked_boxes:
             for y in boxes[x]:
-                if not y in unlocked_boxes and not y > length:
+                if y not in unlocked_boxes and y <= length:
                     unlocked_boxes.append(y)
-    if len(unlocked_boxes) == length:
+    if (len(unlocked_boxes) == length) or\
+            (length == 0 and len(unlocked_boxes) == 1):
         return unlocked
     else:
         return False
