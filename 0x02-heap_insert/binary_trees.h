dc327bc1a7171d9ee7cd1165dbc42f9e42339690
khaldi505 <habetlbrika69@gmail.com>
2021-02-09 15:21:05 +0100

    binary_tree

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -14,10 +14,10 @@
 typedef struct binary_tree_s
 
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 } binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
