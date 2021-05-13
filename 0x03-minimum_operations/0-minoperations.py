aaa3ca1255457675df27b7e1de404e094eabd183
houba <khaldi505>
2021-05-14 00:02:56 +0100

    0-minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+few ops calculator go brerererere
+"""
+
+
+def minOperations(n):
+    op = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            op += i
+            n = int(n / x)
+        x += 1
+    return op
