a6eb536a47e1f9901912d923487ca8e62c269f82
houba <khaldi505>
2021-05-13 23:58:21 +0100

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,12 +4,13 @@
 
 """
 
+
 def minOperations(n):
-	op = 0
-	    x = 2
-	    while n > 1:
-	        while n % x == 0:
-	            op += i
-	            n = int(n / x)
-	        x += 1
-	    return op_count
+    op = 0
+      x = 2
+       while n > 1:
+            while n % x == 0:
+                op += i
+                n = int(n / x)
+            x += 1
+        return op_count
