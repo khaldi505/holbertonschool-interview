923719596f99472e6a6e0c2ddd4ea01e6d4f4d2e
khaldi505 <habetlbrika69@gmail.com>
2021-10-14 12:24:04 +0100

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 
-const request = require('request')
-const filmId = process.argv[2]
-const link = 'https://swapi-api.hbtn.io/api/films/' + filmId
+const request = require('request');
+const filmId = process.argv[2];
+const link = 'https://swapi-api.hbtn.io/api/films/' + filmId;
 
 request(link, async function(error, resp, body){
   const characters = await JSON.parse(body).characters
   for (character of characters) {
     const rep = await new Promise((resolve, reject) => {
       request(character, function(err, rp, bd){
-        resolve(JSON.parse(bd).name)
+        resolve(JSON.parse(bd).name);
       })
     })
-    console.log(rep)
+    console.log(rep);
   }
 })
-
-
