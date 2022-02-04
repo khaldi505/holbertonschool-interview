0ab1809ae020021f67a35c1c4d697295d8de8e2d
khaldi505 <ihebkhaldi505@gmail.com>
2022-02-05 00:38:33 +0100

    0-pascal_triangle.py pep8 validation

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,6 @@ Pascal Triangle
 """
 
 
-
 def pascal_triangle(n):
     """
     Return a list of lists of integers representing the Pascal
@@ -13,5 +12,6 @@ def pascal_triangle(n):
         return []
     result = [[1]]
     for i in range(1, n):
-        result.append([1] + [result[i-1][j] + result[i-1][j+1] for j in range(len(result[i-1])-1)] + [1])
+        result.append([1] + [result[i-1][j] + result[i-1][j+1]
+                      for j in range(len(result[i-1])-1)] + [1])
     return result
