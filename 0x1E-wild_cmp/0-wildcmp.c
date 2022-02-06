8e691307330d01a60ea3c7d398c84c439f038d0e
khaldi505 <ihebkhaldi505@gmail.com>
2022-02-06 21:07:03 +0100

    0-wildcmp. c

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,25 +1,27 @@
 #include "holberton.h"
 
-/**
-* wildcmp - wild compare function
-* @s1: a string
-* @s2: a string
-* Return: 1 if the strings can be considered identical, 0 if not
-*/
+// Prototype: int wildcmp(char *s1, char *s2);
+// s2 can contain the special character *, which matches any sequence of characters.
+// The special char * can replace any string (including an empty string).
 
 int wildcmp(char *s1, char *s2)
 {
-// if *s2 is null, then s1 is null too
 if (*s2 == '\0')
 return (*s1 == '\0');
-// if *s2 is not null, then s1 is not null too
 if (*s1 == '\0')
 return (*s2 == '*');
-// if *s2 is *, then s1 is not null too
 if (*s2 == '*')
 return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2));
-// if *s2 is not *, then s1 is not null too
-if (*s2 != *s1 && *s2 != '?')
+if (*s2 != *s1 && *s2 != '*')
 return (0);
+
+if (*s2 == '*')
 return (wildcmp(s1 + 1, s2 + 1));
+
+
+if (*s2 == *s1)
+return (wildcmp(s1 + 1, s2 + 1));
+
+return (0);
+
 }
