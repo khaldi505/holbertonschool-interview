8e2930b5907d9ad6fb2e882dddda927db5b828bc
khaldi505 <ihebkhaldi505@gmail.com>
2022-02-06 20:54:32 +0100

    0-wildcmp.c

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,49 +1,25 @@
 #include "holberton.h"
 
 /**
-* wildcmp - Compares two string with wild characters
-* @s1: a string 
-* @s2: s2 can contain the special character *.
-* Return:  returns 1 if the strings can be considered identica
+* wildcmp - wild compare function
+* @s1: a string
+* @s2: a string
+* Return: 1 if the strings can be considered identical, 0 if not
 */
 
-int wildcmp(char *s1, char *s2){
-int i, j;
-
-i = 0;
-j = 0;
-
-while (s1[i] != '\0'){
-if (s2[j] == '*'){
-j++;
-while (s2[j] == '*'){
-j++;
-}
-if (s2[j] == '\0'){
-return (1);
-}
-while (s1[i] != '\0'){
-if (wildcmp(s1 + i, s2 + j) == 1){
-return (1);
-}
-i++;
-}
+int wildcmp(char *s1, char *s2)
+{
+// if *s2 is null, then s1 is null too
+if (*s2 == '\0')
+return (*s1 == '\0');
+// if *s2 is not null, then s1 is not null too
+if (*s1 == '\0')
+return (*s2 == '*');
+// if *s2 is *, then s1 is not null too
+if (*s2 == '*')
+return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2));
+// if *s2 is not *, then s1 is not null too
+if (*s2 != *s1 && *s2 != '?')
 return (0);
-}
-if (s1[i] != s2[j]){
-return (0);
-}
-i++;
-j++;
-}
-if (s2[j] == '\0'){
-return (1);
-}
-while (s2[j] == '*'){
-j++;
-}
-if (s2[j] == '\0'){
-return (1);
-}
-return (0);
+return (wildcmp(s1 + 1, s2 + 1));
 }
