8f07e4cfd8ae65772692632a70271cafad4cae97
khaldi505 <ihebkhaldi505@gmail.com>
2022-02-06 20:43:25 +0100

    0-wildcmp.c

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+* wildcmp - Compares two string with wild characters
+* @s1: a string 
+* @s2: s2 can contain the special character *.
+* Return:  returns 1 if the strings can be considered identica
+*/
+
+int wildcmp(char *s1, char *s2){
+int i, j;
+
+i = 0;
+j = 0;
+
+while (s1[i] != '\0'){
+if (s2[j] == '*'){
+j++;
+while (s2[j] == '*'){
+j++;
+}
+if (s2[j] == '\0'){
+return (1);
+}
+while (s1[i] != '\0'){
+if (wildcmp(s1 + i, s2 + j) == 1){
+return (1);
+}
+i++;
+}
+return (0);
+}
+if (s1[i] != s2[j]){
+return (0);
+}
+i++;
+j++;
+}
+
+while (s2[j] == '*'){
+j++;
+}
+if (s2[j] == '\0'){
+return (1);
+}
+return (0);
+}
